fbe6042445aec98078c4fbcc37cc0a26aa074ac3
unknown <e0524723@u.nus.edu>
2025-04-05 11:09:52 +0800

    feat: update sql schemas

--- db/schema.sql
+++ db/schema.sql
@@ -1,37 +1,29 @@
 CREATE TYPE question_type AS ENUM ('multiple-choice', 'open-ended');
 CREATE TYPE subscription_tier AS ENUM ('Explorer', 'Scholar');
 
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    subscription_status subscription_tier NOT NULL
+);
+
 CREATE TABLE questions (
     id SERIAL PRIMARY KEY,
     type question_type NOT NULL,
     question TEXT NOT NULL,
     answer TEXT NOT NULL,
     source TEXT,
-    explanation TEXT
-);
-
-CREATE TABLE multiple_choice_options (
-    id SERIAL PRIMARY KEY,
-    question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
-    value TEXT NOT NULL,
-    text TEXT NOT NULL
+    explanation TEXT,
+    options JSONB
 );
 
 CREATE TABLE answered_questions (
     id SERIAL PRIMARY KEY,
-    question TEXT NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    question_id INTEGER NOT NULL REFERENCES questions(id) ON DELETE CASCADE,
     correct BOOLEAN NOT NULL,
     date TIMESTAMP NOT NULL,
     time_taken INTEGER NOT NULL,
-    user_answer TEXT NOT NULL,
-    correct_answer TEXT NOT NULL,
-    explanation TEXT,
-    source TEXT
-);
-
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    subscription_status subscription_tier NOT NULL
+    user_answer TEXT NOT NULL
 );
